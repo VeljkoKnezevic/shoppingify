227623edff42eced9194ec8d22c3480d383e27c7
VeljkoKnezevic <veljkoknedla@gmail.com>
2024-01-31 17:57:16 +0100

    Initial controllers done

--- server/src/main/java/com/veljkoknezevic/server/controller/ListController.java
+++ server/src/main/java/com/veljkoknezevic/server/controller/ListController.java
@@ -1,11 +1,32 @@
 package com.veljkoknezevic.server.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.veljkoknezevic.server.entity.ShoppingList;
+import com.veljkoknezevic.server.service.ListService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/list")
 public class ListController {
 
+    private final ListService listService;
+
+    public ListController(ListService listService) {
+        this.listService = listService;
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<?> getAllLists() {
+        List<ShoppingList> shoppingLists = listService.getAllLists();
+        return ResponseEntity.ok(shoppingLists);
+    }
+
+    @PostMapping("/new")
+    public ResponseEntity<?> addNewList(@RequestBody ShoppingList shoppingList) {
+        ShoppingList newList = listService.addNewList(shoppingList);
+        return ResponseEntity.ok(newList);
+    }
 
 }
--- server/src/main/java/com/veljkoknezevic/server/service/ListService.java
+++ server/src/main/java/com/veljkoknezevic/server/service/ListService.java
@@ -1,4 +1,32 @@
 package com.veljkoknezevic.server.service;
 
+import com.google.gson.Gson;
+import com.veljkoknezevic.server.entity.Item;
+import com.veljkoknezevic.server.entity.ShoppingList;
+import com.veljkoknezevic.server.repository.ItemRepository;
+import com.veljkoknezevic.server.repository.ListRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ListService {
+
+    private final ListRepository listRepository;
+    private final ItemRepository itemRepository;
+
+    Gson gson = new Gson();
+
+    public ListService(ListRepository listRepository, ItemRepository itemRepository) {
+        this.listRepository = listRepository;
+        this.itemRepository = itemRepository;
+    }
+
+    public List<ShoppingList> getAllLists() {
+        return (List<ShoppingList>) listRepository.findAll();
+    }
+
+    public ShoppingList addNewList(ShoppingList list) {
+        return listRepository.save(list);
+    }
 }
